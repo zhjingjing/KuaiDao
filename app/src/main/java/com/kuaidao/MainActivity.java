25575ba8af343b056d8c76d867867972f1958db4
zhao <18337104423@163.com>
2018-03-22 18:18:56 +0800

    导入讯飞demo

--- app/src/main/java/com/kuaidao/MainActivity.java
+++ app/src/main/java/com/kuaidao/MainActivity.java
@@ -2,12 +2,37 @@ package com.kuaidao;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+    @BindView(R.id.tv_yysb)
+    TextView tvYysb;
+    @BindView(R.id.ll_root)
+    LinearLayout llRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.tv_yysb, R.id.ll_root})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_yysb:
+
+
+                break;
+            case R.id.ll_root:
+                break;
+        }
     }
 }
